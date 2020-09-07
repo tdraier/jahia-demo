15b3f33afdee8dd150b84a81fac9840c490b1b4a
tkhadir <tkhadir.dev@gmail.com>
2020-09-07 14:26:58 +0200

    feature-config-db

--- init.sql
+++ init.sql
@@ -1,10 +1,10 @@
-create database testdb;
+--create database testdb;
 --CREATE USER admin WITH PASSWORD 'admin';
 GRANT ALL PRIVILEGES ON DATABASE "testdb" to admin;
 DROP TABLE IF EXISTS "article";
 DROP SEQUENCE IF EXISTS article_id_seq;
 CREATE SEQUENCE article_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
-CREATE TABLE "public"."article" (
+CREATE TABLE public.article (
     "id" integer DEFAULT nextval('article_id_seq') NOT NULL,
     "title" character varying(200) NOT NULL,
     "content" text NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE "public"."article" (
 DROP TABLE IF EXISTS "editor";
 DROP SEQUENCE IF EXISTS editor_id_seq;
 CREATE SEQUENCE editor_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
-CREATE TABLE "public"."editor" (
+CREATE TABLE public.editor (
     "id" integer DEFAULT nextval('editor_id_seq') NOT NULL,
     "userid" character varying(200) NOT NULL,
     "role" character varying(200) NOT NULL
